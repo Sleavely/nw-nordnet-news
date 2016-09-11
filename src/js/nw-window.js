0442d42907d4069d2844317a9c92755294f1168c
Joakim Hedlund <contact@joakimhedlund.com>
2016-09-11 14:06:41 +0200

    Make maximize do something

--- src/js/nw-window.js
+++ src/js/nw-window.js
@@ -7,6 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
   $('.minimize-app').addEventListener('click', function (event) {
     win.minimize();
   });
+  var maximized = false;
+  $('.maximize-app').addEventListener('click', function (event) {
+    if(!maximized)
+    {
+      win.maximize();
+    }
+    else
+    {
+      win.restore();
+    }
+    maximized = !maximized;
+  });
   $('.close-app').addEventListener('click', function (event) {
     gui.App.quit();
   });
